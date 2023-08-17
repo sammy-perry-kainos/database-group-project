3034b73fead1210e780a6575a2e4209840e56c71
Leonardo Bishop <me@leonardobishop.com>
2023-08-17 14:39:04 +0100

    Move client value averages query into procedure

--- 012-list-client-project-value-averages.sql
+++ 012-list-client-project-value-averages.sql
@@ -1,8 +1,11 @@
-START TRANSACTION;
+DELIMITER $$
+DROP PROCEDURE IF EXISTS list_client_average_values;
+CREATE PROCEDURE list_client_average_values()
+BEGIN
+	SELECT Clients.Name as 'Client', AVG(Projects.Value) as 'Project Average'
+	FROM Clients
+	INNER JOIN Projects USING (ClientID)
+	GROUP BY Clients.ClientID;
+END $$
 
-SELECT Clients.Name as 'Client', AVG(Projects.Value) as 'Project Average'
-FROM Clients
-INNER JOIN Projects USING (ClientID)
-GROUP BY Clients.ClientID;
-
-COMMIT;
+DELIMITER ;
