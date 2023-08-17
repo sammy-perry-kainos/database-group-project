7a4544650723e073112ebd588a135d908fe3a54d
Manpreet Singh Mandair <manpreet.mandair@kainos.com>
2023-08-17 11:50:58 +0100

    edited the create table statement

--- Create-Sales-Employee-Table-PB1.sql
+++ Create-Sales-Employee-Table-PB1.sql
@@ -5,25 +5,12 @@ USE Kainoos_ManpreetM;
 
 -- ProductBacklog1
 CREATE TABLE SalesEmployees (
-	SalesEmployeeID int,
-    Name varchar(70),
-    Salary decimal(10,2), 
-    BankAccountNumber varchar(8),
-    NationalInsuranceNumber char(9),
-    CommissionRate double); 
-    
-ALTER TABLE SalesEmployees 
-	ADD PRIMARY KEY(SalesEmployeeID);
-    
-ALTER TABLE SalesEmployees 
-	MODIFY SalesEmployeeID int AUTO_INCREMENT;
-    
-ALTER TABLE SalesEmployees 
-	MODIFY COLUMN Name varchar(70) NOT NULL,
-    MODIFY COLUMN Salary decimal(10,2) NOT NULL,
-    MODIFY COLUMN BankAccountNumber varchar(8) NOT NULL,
-    MODIFY COLUMN CommissionRate double NOT NULL;
-    ;
+	SalesEmployeeID int AUTO_INCREMENT PRIMARY KEY,
+    Name varchar(70) NOT NULL,
+    Salary decimal(10,2) NOT NULL, 
+    BankAccountNumber varchar(8) NOT NULL,
+    NationalInsuranceNumber char(9) NOT NULL,
+    CommissionRate double NOT NULL); 
     
 COMMIT;    
     
