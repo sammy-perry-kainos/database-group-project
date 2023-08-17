58e3d7dd780373122a1bf8f79e7bb8b437eb202b
sammy-perry-kainos <samuel.perry@kainos.com>
2023-08-17 10:48:32 +0100

    Converted script to transaction

--- 003-create-client.sql
+++ 003-create-client.sql
@@ -1,6 +1,10 @@
+START TRANSACTION;
+
 CREATE TABLE IF NOT EXISTS Clients (
 	ClientID int PRIMARY KEY AUTO_INCREMENT,
     Name varchar(70),
     Address varchar(255),
     PhoneNumber char(11)
 );
+
+COMMIT;
