a3040c94689c3eb583fe3654b14f2c9e0393f305
Leonardo Bishop <me@leonardobishop.com>
2023-08-17 14:34:24 +0100

    Add alter table query to list clients script

--- 009-list-clients.sql
+++ 009-list-clients.sql
@@ -1,16 +1,24 @@
+START TRANSACTION;
+
+ALTER TABLE Clients
+ADD SalesEmployeeID int;
+
+ALTER TABLE Clients
+ADD CONSTRAINT fk_person_order
+FOREIGN KEY (SalesEmployeeID) REFERENCES SalesEmployees(SalesEmployeeID); 
+
+COMMIT;
+
+
 DELIMITER $$
 DROP PROCEDURE IF EXISTS list_clients;
 CREATE PROCEDURE list_clients()
 BEGIN
-	START TRANSACTION;
-
 	SELECT Clients.Name as 'Client', SalesEmployees.Name as 'Sales Employee', GROUP_CONCAT(Projects.Name SEPARATOR ', ') as 'Projects'
 	FROM Clients
 	INNER JOIN SalesEmployees USING (SalesEmployeeID)
 	LEFT JOIN Projects USING (ClientID)
 	GROUP BY Clients.ClientID;
-
-	COMMIT;
 END $$
 
 DELIMITER ;
