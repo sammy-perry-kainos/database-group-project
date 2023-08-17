25f7d2f347e1075b64dae93f5136c111a71c0048
Leonardo Bishop <me@leonardobishop.com>
2023-08-17 14:29:17 +0100

    Restore create client script

--- 003-create-client.sql
+++ 003-create-client.sql
@@ -1,12 +1,10 @@
 START TRANSACTION;
 
 CREATE TABLE IF NOT EXISTS Clients (
-    ClientID int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	ClientID int PRIMARY KEY AUTO_INCREMENT NOT NULL,
     Name varchar(70) NOT NULL,
     Address varchar(255) NOT NULL,
-    PhoneNumber char(11) NOT NULL,
-    SalesEmployeeID int NOT NULL,
-    FOREIGN KEY(SalesEmployeeID) REFERENCES SalesEmployees(SalesEmployeeID)
+    PhoneNumber char(11) NOT NULL
 );
 
 COMMIT;
