350472ef2102a30a2a48973bc21ee374c5f0ee72
Manpreet Singh Mandair <manpreet.mandair@kainos.com>
2023-08-17 10:00:53 +0100

    HR Team should be able to create a new sales employee now

---
+++ Create-Sales-Employee-Table-PB1.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE Kainoos_ManpreetM;
+
+USE Kainoos_ManpreetM;
+
+-- ProductBacklog1
+CREATE TABLE SalesEmployees (
+	SalesEmployeeID int,
+    Name varchar(70),
+    Salary decimal(10,2), 
+    BankAccountNumber varchar(8),
+    NationalInsuranceNumber char(9),
+    CommissionRate float); 
+    
+    
+    
+    
