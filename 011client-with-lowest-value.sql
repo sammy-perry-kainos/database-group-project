fca1405b689129732f67442a324e266c3c25c589
Manpreet Singh Mandair <manpreet.mandair@kainos.com>
2023-08-17 14:49:58 +0100

    clients with lowest value of projects

---
+++ 011client-with-lowest-value.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+DROP PROCEDURE IF EXISTS get_lowest_value_client;
+CREATE PROCEDURE get_lowest_value_client()
+BEGIN
+
+	SELECT Clients.Name 
+    FROM Clients
+    JOIN Projects USING (ClientID)
+    GROUP BY Clients.Name
+    ORDER BY SUM(Projects.Value) ASC
+    LIMIT 1;
+
+END $$
+
+DELIMITER ; 
+
+CALL get_lowest_value_client();
