38661d48bcf87af31407f65817e9a12e55ba5393
Leonardo Bishop <me@leonardobishop.com>
2023-08-17 10:00:15 +0100

    Update data types precision

--- 002-create-delivery-employee-table.sql
+++ 002-create-delivery-employee-table.sql
@@ -1,11 +1,11 @@
 START TRANSACTION;
 
 CREATE TABLE DeliveryEmployees (
-    DeliveryEmployeeID      int,
-    Name                    varchar(70),
-    Salary                  decimal,
-    BankAccountNumber       varchar(34),
-    NationalInsuranceNumber char(9),
+    DeliveryEmployeeID      int AUTO_INCREMENT NOT NULL,
+    Name                    varchar(70) NOT NULL, 
+    Salary                  decimal(10, 2) NOT NULL,
+    BankAccountNumber       varchar(8) NOT NULL, 
+    NationalInsuranceNumber char(9) NOT NULL,
     PRIMARY KEY (DeliveryEmployeeID)
 );
 
