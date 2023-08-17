f307f9f27e6839c47a86101f6305b9509ee07f23
Manpreet Singh Mandair <manpreet.mandair@kainos.com>
2023-08-17 12:01:12 +0100

    added clientID to projects table

--- create-projects-tables.sql
+++ create-projects-tables.sql
@@ -6,7 +6,12 @@ CREATE TABLE Projects (
 	ProjectID int AUTO_INCREMENT PRIMARY KEY,
     Name varchar(100) NOT NULL,
     Value decimal(15,2) NOT NULL,
-    TechLead int NOT NULL); 
+    TechLead int NOT NULL,
+    ClientID int NOT NULL );
+    
+ALTER TABLE Projects 
+		ADD CONSTRAINT fk_clients 
+		FOREIGN KEY (ClientID) REFERENCES Clients(ClientID);
     
     
 -- Technologies table
